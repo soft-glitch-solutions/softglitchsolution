0447a461a9634b9b8fd83b760c6ff09e5c5bfa13
Shaqeel Less <54390756+MogammadShaqeelless16@users.noreply.github.com>
2024-12-14 20:33:37 +0000

    update changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ export default function Navbar() {
             <div className="flex-shrink-0 flex items-center space-x-2">
               {/* Replace Sparkles with SVG logo */}
               <img 
-                src="../../SoftGlitch.svg" 
+                src="../assets/SoftGlitch.svg" 
                 alt="Soft Glitch Logo" 
                 className="h-8 w-8" 
               />
